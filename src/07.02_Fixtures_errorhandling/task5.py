710bf206caa4c0ae83b9bca4ec928d26c5e0f59b
Gintautas Lukšas <gintautas.luksas@gmail.com>
2024-07-04 18:10:46 +0300

    Update task5.py

--- src/07.02_Fixtures_errorhandling/task5.py
+++ src/07.02_Fixtures_errorhandling/task5.py
@@ -1,12 +1,9 @@
 import pytest
 
-
-# Function to get value from dictionary by key
 def get_value_dict(d, key):
     return d.get(key, None)
 
 
-# Function to remove key-value pair from dictionary
 def remove_value_dict(d, key):
     if key in d:
         del d[key]
@@ -14,9 +11,9 @@ def remove_value_dict(d, key):
 
 @pytest.fixture
 def dict_data():
-    # Updated sample dictionary and key for testing
+
     dictionary = {'a': 1, 'c': 3}
-    key = 'b'  # Note: 'b' key does not exist in dictionary
+    key = 'b'
     return dictionary, key
 
 
@@ -24,7 +21,6 @@ def dict_data():
 def test_get_value_dict(dict_data):
     dictionary, key = dict_data
     assert get_value_dict(dictionary, key) == None  # Ensure key 'b' does not exist
-
 
 
 def test_remove_value_dict(dict_data):
