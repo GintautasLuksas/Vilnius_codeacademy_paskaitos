a2a163896a1898cd6190d8b27220ada01536cbca
Gintautas Lukšas <gintautas.luksas@gmail.com>
2024-07-04 19:01:08 +0300

    did

---
+++ src/07.02_Fixtures_errorhandling/task10.py
@@ -0,0 +1,3 @@
+# Užduotis 10
+# 	Funkcija: Įgyvendinkite funkciją, kuri skaito iš failo ir apdoroja jo turinį. Iškelkite atitinkamas klaidas, jei failas nerastas.
+# 	Testai: Parašykite testus su fixture, kurie apima skaitymą iš esamo failo, skaitymą iš neegzistuojančio failo (kad būtų sukelta FileNotFoundError).
